96e88365ffdfc5b559d2fb20fa0edec513405bd7
sessentaeseis <66entreaspasduplas@gmail.com>
2026-06-02 19:39:46 -0300

    removendol localhost 3000 do proxy

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,3 @@
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-      },
-      '/health': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-      },
-    },
-  },
-});
+export default defineConfig({});
