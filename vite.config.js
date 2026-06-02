eb58dec44f285f637a6c0af1ce79b83f45a69ee7
sessentaeseis <66entreaspasduplas@gmail.com>
2026-06-02 20:11:30 -0300

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,16 @@
 import { defineConfig } from 'vite';
 
-export default defineConfig({});
+export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      },
+      '/health': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      },
+    },
+  },
+});
